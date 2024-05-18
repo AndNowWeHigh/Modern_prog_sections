3c7c94a0b8d1e70445c006646523fe5c873de0ac
AndNowWeHigh <145552175+AndNowWeHigh@users.noreply.github.com>
2024-05-18 23:42:04 +0300

    Update test_lab_1_2.py
    Зміни

--- Lab1_2/test_lab_1_2.py
+++ Lab1_2/test_lab_1_2.py
@@ -1,18 +1,41 @@
 import unittest
+from io import StringIO
+from unittest.mock import patch
+import sys
 from lab_1_2 import count_occurrences
 
 class MyTestCase(unittest.TestCase):
-    def test_task(self):
-        self.assertEqual(count_occurrences("world hello world", "world"), 2)
 
-    def test_empty_word(self):
-        self.assertEqual(count_occurrences("", "hello"), 0)
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_empty_text(self, mock_stderr):
+        with patch('sys.stdin', side_effect=["\n", "hello\n"]):
+            with self.assertRaises(SystemExit) as cm:
+                count_occurrences("", "hello")
+        self.assertEqual(cm.exception.code, 1)
+        self.assertEqual(mock_stderr.getvalue().strip(), "Нема тексту")
 
-    def test_word_empty(self):
-        self.assertEqual(count_occurrences("hello world hello", ""), 0)
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_empty_search_word(self, mock_stderr):
+        with patch('sys.stdin', side_effect=["hello\n", "\n"]):
+            with self.assertRaises(SystemExit) as cm:
+                count_occurrences("hello world", "")
+        self.assertEqual(cm.exception.code, 1)
+        self.assertEqual(mock_stderr.getvalue().strip(), "Нема слова, яке потрібно найти")
 
-    def test_invalid_word(self):
-        self.assertEqual(count_occurrences('171 1 71 17 1', '1'), 2)
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_error_stream(self, mock_stderr):
+        with patch('sys.stdin', side_effect=["\n", "hello\n"]):
+            with self.assertRaises(SystemExit):
+                count_occurrences("", "hello")
+        self.assertNotEqual(mock_stderr.getvalue().strip(), "")
+        self.assertEqual(mock_stderr.getvalue().strip(), "Нема тексту")
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_correct_output(self, mock_stdout):
+        result = count_occurrences("hello world hello", "hello")
+        print(result, file=sys.stdout)
+        self.assertEqual(mock_stdout.getvalue().strip(), "Кількість:2")
+
 
 if __name__ == '__main__':
     unittest.main()
