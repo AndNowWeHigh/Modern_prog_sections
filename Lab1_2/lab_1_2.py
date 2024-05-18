6691bc676d0b47d0214a5db39e87d806c9464164
AndNowWeHigh <145552175+AndNowWeHigh@users.noreply.github.com>
2024-05-18 23:41:05 +0300

    Update lab_1_2.py
    Зміни

--- Lab1_2/lab_1_2.py
+++ Lab1_2/lab_1_2.py
@@ -1,17 +1,19 @@
 import sys
 
 def count_occurrences(text, word):
+    if not text:
+        print("Нема тексту", file=sys.stderr)
+        sys.exit(1)
+
+    if not word:
+        print("Нема слова, яке потрібно найти", file=sys.stderr)
+        sys.exit(1)
     words = text.split()
     count = words.count(word)
-
-    return count
+    return f"Кількість:{count}"
 
 if __name__ == "__main__":
-    # input
     text = sys.stdin.readline().strip()
-    # search
     search_word = sys.stdin.readline().strip()
-    # count
-    occurrences = count_occurrences(text, search_word)
-    # output
-    print(occurrences, file=sys.stdout)
+
+    print(count_occurrences(text, search_word), file=sys.stdout)
